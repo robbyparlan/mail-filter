a03b6286bf8c74ae1975af1cfc55490f401048e5
ruitze <36460501+robbyparlan@users.noreply.github.com>
2018-03-07 22:58:20 +0700

    Create mailfilter.py

---
+++ mailfilter.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import os.path
+import re
+
+class warna():
+    cetak = '\033[0m'
+    ijo ='\033[1;32m'
+    putih = '\033[1;37m'
+    biru = '\033[1;34m'
+
+def author():
+    print('''%s
+______  ___      ___________________________________
+___   |/  /_____ ___(_)__  /___  ____/__(_)__  /_  /_____________
+__  /|_/ /_  __ `/_  /__  / __  /_   __  /__  /_  __/  _ \_  ___/
+_  /  / / / /_/ /_  / _  /___  __/   _  / _  / / /_ /  __/  /
+/_/  /_/  \__,_/ /_/  /_____/_/      /_/  /_/  \__/ \___//_/
+                                                       v.01
+%s
+[+] Author      : Ruitze
+[+] Tool        : MailFilter v.01
+[+] Description : Filter mailist Gmail, Yahoo, Hotmail
+[+] Usage       : python mailFilter.py nameMailist -o %s''' % (warna.ijo,warna.putih, warna.cetak))
+
+
+regex = re.compile(r'''(
+                   ([a-zA-Z0-9._%+-]+)
+                    @
+                    (gmail|yahoo|hotmail)
+                    (\.com)
+                    )''', re.VERBOSE)
+
+def file_to_str(filename):
+    with open(filename) as f:
+        return f.read().lower()
+
+def get_emails(s):
+    return (email[0] for email in re.findall(regex, s) if not email[0].startswith('//'))
+
+def main():
+    parser = OptionParser()
+    parser.add_option('-o','--output',dest='output',help='output file to saveFile.txt', action='store_true')
+    (options, args) = parser.parse_args()
+    try:
+        #if not args:
+         #   author()
+          #  print(warna.cetak)
+        if args and options.output:
+            for arg in args:
+                if os.path.isfile(arg):
+                    for email in get_emails(file_to_str(arg)):
+                        f =open('saveFile.txt', 'a')
+                        f.write(email+'\n')
+                        f.close()
+                    print('%sSucces to saveFile.txt%s' % (warna.biru, warna.cetak))
+                else:
+                    print('"{}" is not a file.').format(arg)
+
+        else:
+            author()
+            print(warna.cetak)
+    except Exception as e:
+        print('Error Description', e)
+
+
+if __name__=='__main__':
+    main()
